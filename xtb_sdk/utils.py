90999731a3f05bb181e7df400b20e9b1b7a9cc5b
bwrob <bw2002@wp.pl>
2024-04-13 23:52:21 +0200

    add mypy to pre-commit hooks

--- xtb_sdk/utils.py
+++ xtb_sdk/utils.py
@@ -1,5 +1,6 @@
 """Utility functions."""
 
+from io import StringIO
 from typing import Callable
 
 import pandas as pd
@@ -19,9 +20,14 @@ def inspect_dataframe(
         data (pd.DataFrame): The DataFrame to inspect.
 
     """
+    # info prints to sys.stdout, provide buffer to capture output
+    buffer = StringIO()
+    data.info(buf=buffer)
+    info_string = buffer.getvalue()
+
     inspect_msg = (
         f"Dataframe head: \n {data.head()} \n"
-        f"Dataframe summary: \n {data.info()} \n"
+        f"Dataframe summary: \n {info_string} \n"
         f"Dataframe descriptive statistics: \n {data.describe()}\n"
     )
     stream(inspect_msg)
