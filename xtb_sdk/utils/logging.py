3a01e4e4fb306d36121222877547eede905dec75
bwrob <bw2002@wp.pl>
2024-04-20 19:29:48 +0200

    global debug mode

--- xtb_sdk/utils/logging.py
+++ xtb_sdk/utils/logging.py
@@ -2,12 +2,27 @@
 
 import logging
 
-LOGGING_FORMAT = "[%(asctime)-15s][%(funcName)s:%(lineno)d] %(message)s"
-LOGGER_NAME = "xtb_logger"
+__LOGGING_FORMAT = "[%(asctime)-15s][%(funcName)s:%(lineno)d] %(message)s"
+__LOGGER_NAME = "xtb"
+__LOGGER_NAME_WEBSOCKETS_CLIENT = "websockets.client"
+__LOGGER_NAME_WEBSOCKETS_SERVER = "websockets.server"
+__LOGGER_LEVEL = logging.INFO
+
+logging.basicConfig(format=__LOGGING_FORMAT, level=__LOGGER_LEVEL)
 
 
 def get_logger() -> logging.Logger:
     """Provides a default logger for use in the package."""
-    logger = logging.getLogger(LOGGER_NAME)
-    logging.basicConfig(format=LOGGING_FORMAT, level=logging.DEBUG)
+    logger = logging.getLogger(__LOGGER_NAME)
+    logging.basicConfig(format=__LOGGING_FORMAT, level=logging.DEBUG)
     return logger
+
+
+def enable_debug_mode():
+    """Enable debug mode."""
+    for name in (
+        __LOGGER_NAME,
+        __LOGGER_NAME_WEBSOCKETS_CLIENT,
+        __LOGGER_NAME_WEBSOCKETS_SERVER,
+    ):
+        logging.getLogger(name).setLevel(logging.DEBUG)
